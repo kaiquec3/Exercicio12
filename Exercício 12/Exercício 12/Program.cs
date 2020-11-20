393d96800cb99125c29b03acae4c37e4ceb600e6
kaiquec3 <74798358+kaiquec3@users.noreply.github.com>
2020-11-20 20:39:50 -0300

    Add files via upload

---
+++ Exercício 12/Exercício 12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercício_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new Triangulo();
+            double k;
+
+            do
+            {
+                Console.Write("Informe o primeiro lado do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado1(k);
+            }
+            while (k <= 0);
+
+            do
+            {
+                Console.Write("Informe o segundo lado do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado2(k);
+            }
+            while (k <= 0);
+
+            do
+            {
+                Console.Write("Informe o terceiro lado do triângulo: ");
+                k = double.Parse(Console.ReadLine());
+
+                if (k > 0) a1.SetLado3(k);
+            }
+            while (k <= 0);
+
+            if (a1.VerificarSeETriangulo() == 1)
+            {
+                Console.WriteLine($"Estes lados formam um triângulo {a1.DeterminarTipoDeTriangulo()}");
+            }
+            else Console.WriteLine(a1.DeterminarTipoDeTriangulo());
+
+        }
+    }
+}
